65e312ff39ac5be31ba4268cfdca430717f41d2e
KaliStark <kalistark@localhost.localdomain>
2021-06-15 17:20:11 +0530

    Login redirection problem solved

--- DjangoEcommerceApp/views.py
+++ DjangoEcommerceApp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import AnonymousUser
 from django.contrib import messages
 from django.urls import reverse
 
@@ -12,11 +13,23 @@ def demoPageTemplate(req):
     return render(req,'demo.html')
 
 def adminLogin(req):
+    is_authenticated_user = authenticate(req.user)
+    print("\nIs authenticated user:",is_authenticated_user,end="\n\n")
+    print("\n\nThe present user is:",req.user,end="\n\n")
+    print("The present user is not AnonymousUser:",(req.user.__str__() is not "AnonymousUser"),end="\n\n")
+    print(req.user.__str__())
+    # if is_authenticated_user:
+    if (req.user.__str__() != "AnonymousUser") and (req.user != None):
+        print("The user is autheticated to enter the adminHome")
+        return HttpResponseRedirect(reverse('adminHome'))
     return render(req,'admin_templates/signin.html')
 
 def adminLoginProcess(req):
-    username = req.POST['username']
-    password = req.POST['password']
+    try:
+        username = req.POST['username']
+        password = req.POST['password']
+    except:
+        return HttpResponseRedirect(reverse('Admin Login'))
     user = authenticate(request=req,username=username,password=password)
     if user is not None:
         login(request=req,user=user)
